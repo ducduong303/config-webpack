64fb9bca3f636b13f08b47938b4441cf9403a7db
ducduong303 <57469702+ducduong303@users.noreply.github.com>
2021-08-27 11:21:55 +0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
-import "./test"
-
-console.log("index");
+// import "./test"
+// console.log("index");
